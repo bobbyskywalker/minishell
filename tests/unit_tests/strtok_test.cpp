a4f9501169e665b329acbad479d67be8efe13621
bobbyskywalker <aleksander.garbacz@gmail.com>
2025-01-28 19:43:57 +0100

    strotk wip

--- tests/unit_tests/strtok_test.cpp
+++ tests/unit_tests/strtok_test.cpp
@@ -22,3 +22,51 @@ TEST(StrtokTest, ValidStrtok)
     token = ft_strtok(str, ' ');
     EXPECT_EQ(token, nullptr);
 }
+
+TEST(StrtokTest, EmptyStrtok)
+{
+    const char *str = "";
+    char *token = ft_strtok(str, ' ');
+    EXPECT_EQ(token, nullptr);
+}
+
+TEST(StrtokTest, NullStrtok)
+{
+    char *token = ft_strtok(nullptr, ' ');
+    EXPECT_EQ(token, nullptr);
+}
+
+TEST(StrtokTest, ShellLikeStrtok)
+{
+    ft_strtok_reset();
+    char *str = "echo 123\"super\" '|' grep '1' | wc\"-l\"";
+    char *token = ft_strtok(str, ' ');
+    EXPECT_STREQ(token, "echo");
+    free(token);
+    token = ft_strtok(str, ' ');
+    EXPECT_STREQ(token, "123");
+    free(token);
+    token = ft_strtok(str, ' ');
+    EXPECT_STREQ(token, "\"super\"");
+    free(token);
+    token = ft_strtok(str, ' ');
+    EXPECT_STREQ(token, "|");
+    free(token);
+    token = ft_strtok(str, ' ');
+    EXPECT_STREQ(token, "grep");
+    free(token);
+    token = ft_strtok(str, ' ');
+    EXPECT_STREQ(token, "'1'");
+    free(token);
+    token = ft_strtok(str, ' ');
+    EXPECT_STREQ(token, "|");
+    free(token);
+    token = ft_strtok(str, ' ');
+    EXPECT_STREQ(token, "wc");
+    free(token);
+    token = ft_strtok(str, ' ');
+    EXPECT_STREQ(token, "\"-l\"");
+    free(token);
+    token = ft_strtok(str, ' ');
+    EXPECT_EQ(token, nullptr);
+}
