c429adf7969b76498ac087af6c820d3a1161a3b1
Aleksander Garbacz <agarbacz@c3r11s3.42warsaw.pl>
2025-01-28 19:43:57 +0100

    strotk wip

--- inc/minishell.h
+++ inc/minishell.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: agarbacz <agarbacz@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/28 18:09:13 by agarbacz          #+#    #+#             */
+/*   Updated: 2025/01/28 19:41:52 by agarbacz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef MINISHELL_H
 #define MINISHELL_H
 
@@ -39,6 +51,7 @@ void execute_command(char *command);
 
 // SECTION: lexer
 char	*ft_strtok(const char *str, char separator);
+void    ft_strtok_reset(void);
 
 // SECTION: built-in commands
 int ft_echo(void);
--- src/parser/lexer.c
+++ src/parser/lexer.c
@@ -1,36 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lexer.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: agarbacz <agarbacz@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/28 18:08:51 by agarbacz          #+#    #+#             */
+/*   Updated: 2025/01/28 19:41:40 by agarbacz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../../inc/minishell.h"
+#include <assert.h>
+
+void	ft_strtok_reset(void)
+{
+	static int	i = 0;
+
+	i = 0;
+}
 
 char	*ft_strtok(const char *str, char separator)
 {
-    static int i = 0;
-    int j;
-    int len;
-    char *token;
+	static int	i = 0;
+	int			j;
+	int			len;
+	char		*token;
 
-    if (i == ft_strlen(str))
-        return (NULL);
-    while(str[i] && str[i] == separator)
-        i++;
-    len = 0;
-    while (str[len + i] && str[len + i] != separator)
-        len++;
-    token = malloc((len + 1) * sizeof(char));
-    if (!token)
-        return (NULL);
-    j = 0;
-    while ((str[i]) != separator)
-        token[j++] = str[i++];
-    token[j] = '\0';
-    return (token);
+	if (i == (int)ft_strlen(str))
+		return (NULL);
+	while (str[i] && str[i] == separator)
+		i++;
+	len = 0;
+	while (str[len + i] && str[len + i] != separator)
+		len++;
+	token = malloc((len + 1) * sizeof(char));
+	if (!token)
+		return (NULL);
+	j = 0;
+	while ((str[i]) != separator && str[i] != '\0')
+		token[j++] = str[i++];
+	token[j] = '\0';
+	return (token);
 }
-
-// int main()
-// {
-//     const char *str = "hello world";
-//     char *token = ft_strtok(str, ' ');
-//     EXPECT_STREQ(token, "hello");
-//     token = ft_strtok(NULL, ' ');
-// }
 
 // char **tokenize(char *cmd)
 // {
