955f684efa6696cca5b040015075689370b8f89d
Olek <aleksander.garbacz@gmail.com>
2025-01-29 19:56:01 +0100

    sicc af

--- src/parser/lexer.c
+++ src/parser/lexer.c
@@ -11,38 +11,49 @@
 /* ************************************************************************** */
 
 #include "../../inc/minishell.h"
-#include <assert.h>
 
-void	ft_strtok_reset(void)
-{
-	static int	i = 0;
-
-	i = 0;
-}
-
+// undefined behaviour for closing double quote with single quote & vice versa
 char	*ft_strtok(const char *str, char separator)
 {
 	static int	i = 0;
 	int			j;
 	int			len;
 	char		*token;
+    bool         in_quote;
 
-	if (i == (int)ft_strlen(str))
+    in_quote = false;
+	if (!str || !str[0] || i == (int)ft_strlen(str))
+    {
+        i = 0;
 		return (NULL);
+    }
 	while (str[i] && str[i] == separator)
 		i++;
-	len = 0;
-	while (str[len + i] && str[len + i] != separator)
+    if ((str[i] == '\'' || str[i] == '\"') && str[i - 1] == ' ')
+        in_quote = true;
+    len = 0;
+	while (str[len + i] && ((str[len + i] != separator && in_quote == false) 
+        || ((str[len + i] == '\'') || ((str[len + i] == '\"') && in_quote))))
 		len++;
 	token = malloc((len + 1) * sizeof(char));
 	if (!token)
 		return (NULL);
 	j = 0;
-	while ((str[i]) != separator && str[i] != '\0')
+    while (str[i] && ((str[i] != separator && in_quote == false) 
+        || ((str[i] == '\'') || ((str[i] == '\"') && in_quote))))
 		token[j++] = str[i++];
 	token[j] = '\0';
 	return (token);
 }
+
+// int main()
+// {
+//     char *cmd = "echo 123\"super\"";
+//     char *token = ft_strtok(cmd, ' ');
+//     printf("%s\n", token);
+//     free(token);
+//     return (0);
+// }
 
 // char **tokenize(char *cmd)
 // {
