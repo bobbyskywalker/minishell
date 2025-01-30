d9e802f53c5044c474bded6180c975c4b0a90cfe
jzackiew <jzackiew@student.42warsaw.pl>
2025-01-30 15:46:15 +0100

    made echo and pwd

--- src/built_ins.c
+++ src/built_ins.c
@@ -3,53 +3,87 @@
 /*                                                        :::      ::::::::   */
 /*   built_ins.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: agarbacz <agarbacz@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jzackiew <jzackiew@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/28 18:08:59 by agarbacz          #+#    #+#             */
-/*   Updated: 2025/01/28 18:09:00 by agarbacz         ###   ########.fr       */
+/*   Updated: 2025/01/30 15:41:26 by jzackiew         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
 
-int ft_echo(void)
+int	ft_echo(t_ast_node node)
 {
-  printf("here is echo command\n");
-  return (0);
+	size_t	i;
+	int		is_flag;
+
+	is_flag = 0;
+	
+	i = 0;
+	if (!ft_strncmp(node.command->args[0], "-n", 3))
+	{
+		is_flag = 1;
+		i++;
+	}
+	while (node.command->args[i])
+	{
+		ft_printf("%s", node.command->args[i]);
+		i++;
+	}
+	if (!is_flag)
+		ft_printf("\n");
+	return (0);
 }
 
-int ft_cd(void)
+int	ft_cd(void)
 {
 	printf("here is cd command\n");
-  return (0);
+	return (0);
 }
 
-int ft_pwd(void)
+int	ft_pwd(void)
 {
-  printf("here is the pwd command\n");
-  return (0);
+	char	*pwd_str;
+	
+	pwd_str = getcwd(NULL, 99);
+	ft_printf("%s\n", pwd_str);
+	return (0);
 }
 
-int ft_exit(void)
+int	ft_exit(void)
 {
-  printf("here is exit command\n");
-  return (0);
+	printf("here is exit command\n");
+	return (0);
 }
 
-int ft_export(void)
+int	ft_export(void)
 {
-  printf("here is export command\n");
-  return (0);
+	printf("here is export command\n");
+	return (0);
 }
 
-int ft_unset(void)
+int	ft_unset(void)
 {
-  printf("here is the unset command\n");
-  return (0);
+	printf("here is the unset command\n");
+	return (0);
 }
 
-int ft_env(void)
+int	ft_env(void)
 {
-    printf("here is the env command\n");
-    return (0);
+	printf("here is the env command\n");
+	return (0);
+}
+
+int	main(void)
+{
+	t_ast_node node;
+	char *tmp[] = {"dupa ", "i ", "chuj", NULL};
+
+	node.command = malloc(sizeof(t_command));
+	if (!node.command)
+		return (1);
+	node.command->args = ft_2d_strdup(tmp);
+	ft_echo(node);
+	ft_pwd();
+	return (1);
 }
