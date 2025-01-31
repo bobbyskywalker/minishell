75f77a059ffa16ebfc9985a66e1e21bc41f3ecf5
Jzackiewicz <jj.zackiewicz@gmail.com>
2025-01-31 18:46:00 +0100

    pogrzebał i zostawił

--- inc/minishell.h
+++ inc/minishell.h
@@ -6,24 +6,23 @@
 /*   By: jzackiew <jzackiew@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/28 18:09:13 by agarbacz          #+#    #+#             */
-/*   Updated: 2025/01/31 10:52:20 by jzackiew         ###   ########.fr       */
-/*   Updated: 2025/01/30 15:33:30 by agarbacz         ###   ########.fr       */
+/*   Updated: 2025/01/31 16:22:26 by jzackiew         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-#define _GNU_SOURCE
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <stdbool.h>
-# include <signal.h>
-#include "../lib/libft/libft.h"
-#include "../lib/libft/gnl/get_next_line.h"
+# define _GNU_SOURCE
+# include "../lib/libft/gnl/get_next_line.h"
+# include "../lib/libft/libft.h"
 # include "parser.h"
+# include <fcntl.h>
+# include <signal.h>
+# include <stdbool.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
 
 // AST node type enumerations
 typedef enum e_node_type
@@ -49,19 +48,26 @@ typedef struct s_ast_node
 	t_command			*command;
 }						t_ast_node;
 
+typedef struct	s_shell_data
+{
+	char	**env_vars;
+	int		last_cmd_status;
+}				t_shell_data;
+
+
 char					**split_line(char *line);
 void					execute_command(char *command);
 
 // SECTION: lexer
-char	**tokenize(char *source);
+char					**tokenize(char *source);
 
 // SECTION: built-in commands
-int	ft_echo(t_ast_node node);
-int	ft_cd(t_ast_node node);
-int ft_pwd(void);
-int ft_exit(void);
-int	ft_export(t_ast_node node);
-int ft_unset(void);
-int ft_env(void);
+int						ft_echo(t_ast_node node);
+int						ft_cd(t_ast_node node);
+int						ft_pwd(void);
+int						ft_exit(void);
+void					ft_export(t_ast_node node, t_shell_data *shell_data);
+int						ft_unset(void);
+int						ft_env(void);
 
 #endif
