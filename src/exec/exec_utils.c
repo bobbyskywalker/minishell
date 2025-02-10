71c886da0687fb7779cdcaaf4c4f15b33544c863
Aleksander Garbacz <agarbacz@c4r1s2.42warsaw.pl>
2025-02-10 18:01:06 +0100

    commented conflicting types, minor fixes

--- src/exec/exec_utils.c
+++ src/exec/exec_utils.c
@@ -1,25 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   exec_utils.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: agarbacz <agarbacz@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/10 17:50:45 by agarbacz          #+#    #+#             */
+/*   Updated: 2025/02/10 17:55:35 by agarbacz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../../inc/built_ins.h"
 #include "../../inc/minishell.h"
 
 // nie można wywołać ./minishell
-int	is_builtin(t_ast_node node, t_shell_data *shell_data)
+// int	is_builtin(t_ast_node node, t_shell_data *shell_data)
+// {
+// 	if (!ft_strncmp(node.command->args[0], "echo", 4))
+// 		return (ft_echo(&node.command->args[1]));
+// 	if (!ft_strncmp(node.command->args[0], "cd", 2))
+// 		return (ft_cd(&node.command->args[1], *shell_data));
+// 	if (!ft_strncmp(node.command->args[0], "pwd", 3))
+// 		return (ft_pwd());
+// 	if (!ft_strncmp(node.command->args[0], "exit", 4))
+// 		return (ft_exit());
+// 	if (!ft_strncmp(node.command->args[0], "export", 6))
+// 		return (ft_export(&node.command->args[1], shell_data));
+// 	if (!ft_strncmp(node.command->args[0], "unset", 5))
+// 		return (ft_unset(&node.command->args[1], shell_data));
+// 	if (!ft_strncmp(node.command->args[0], "env", 3))
+// 		return (ft_env(*shell_data));
+// 	return (0);
+// }
+
+int	is_builtin(char *cmd)
 {
-	if (!ft_strncmp(node.command->args[0], "echo", 4))
-		return (ft_echo(&node.command->args[1]));
-	if (!ft_strncmp(node.command->args[0], "cd", 2))
-		return (ft_cd(&node.command->args[1], *shell_data));
-	if (!ft_strncmp(node.command->args[0], "pwd", 3))
-		return (ft_pwd());
-	if (!ft_strncmp(node.command->args[0], "exit", 4))
-		return (ft_exit());
-	if (!ft_strncmp(node.command->args[0], "export", 6))
-		return (ft_export(&node.command->args[1], shell_data));
-	if (!ft_strncmp(node.command->args[0], "unset", 5))
-		return (ft_unset(&node.command->args[1], shell_data));
-	if (!ft_strncmp(node.command->args[0], "env", 3))
-		return (ft_env(*shell_data));
+	if (!ft_strncmp(cmd, "echo", 4))
+		return (1);
+	if (!ft_strncmp(cmd, "cd", 2))
+		return (1);
+	if (!ft_strncmp(cmd, "pwd", 3))
+		return (1);
+	if (!ft_strncmp(cmd, "exit", 4))
+		return (1);
+	if (!ft_strncmp(cmd, "export", 6))
+		return (1);
+	if (!ft_strncmp(cmd, "unset", 5))
+		return (1);
+	if (!ft_strncmp(cmd, "env", 3))
+		return (1);
 	return (0);
 }
+
 
 // return non-zero on error
 int	prepare_cmd_for_exec(t_ast_node *node, char **envp)
