5a16c6c179a918786a581e4ffc53fb0627e9e609
bobbyskywalker <aleksander.garbacz@gmail.com>
2025-02-11 11:52:14 +0100

    shell data swap

--- inc/minishell.h
+++ inc/minishell.h
@@ -19,6 +19,9 @@
 # include <sys/wait.h>
 # include <unistd.h>
 
+// SECTION: lexer
+char		**tokenize(char *source);
+
 // SECTION: redirections
 void		restore_fds(int saved_stdin, int saved_stdout);
 t_ast_node	*traverse_to_command(t_ast_node *node);
@@ -30,18 +33,16 @@ int			handle_heredoc(char *limiter);
 
 // SECTION: pipes
 int			kill_on_error(pid_t pid, int mode);
-void		exec_first_child(int pipe_fd[2], t_ast_node *node, char **envp);
-void		exec_second_child(int pipe_fd[2], t_ast_node *node, char **envp);
+void		exec_first_child(int pipe_fd[2], t_ast_node *node, t_shell_data *shell_data);
+void		exec_second_child(int pipe_fd[2], t_ast_node *node, t_shell_data *shell_data);
 void		close_and_await(int pipe_fd[2], int pid1, int pid2, int status);
-// SECTION: lexer
-char		**tokenize(char *source);
 
 // SECTION: execution
 char		**get_path_env_var(char **envp);
 char		*validate_command(char *cmd, char **dirs);
 int			prepare_cmd_for_exec(t_ast_node *node, char **envp);
 int			calc_file_flags(t_ast_node *node);
-int			execute_ast(t_ast_node *node, char **envp);
+int			execute_ast(t_ast_node *node, t_shell_data *shell_data);
 
 // SECTION: exec utils
 // int			is_builtin(t_ast_node node, t_shell_data *shell_data);
