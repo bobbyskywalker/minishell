91ea33a38508cc6a4a608e50ec113ecb45ed8fd4
Aleksander Garbacz <agarbacz@c4r1s5.42warsaw.pl>
2025-02-11 11:52:14 +0100

    shell data swap

--- src/exec/execute.c
+++ src/exec/execute.c
@@ -6,7 +6,7 @@
 /*   By: agarbacz <agarbacz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/28 18:09:01 by agarbacz          #+#    #+#             */
-/*   Updated: 2025/02/10 16:59:25 by agarbacz         ###   ########.fr       */
+/*   Updated: 2025/02/11 11:50:04 by agarbacz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,17 +20,17 @@
 // returns negative value on errors:
 // - tree non-existent
 // - command execution failure
-int	execute_command(t_ast_node *node, char **envp)
+int	execute_command(t_ast_node *node, t_shell_data *shell_data)
 {
 	int		status;
 	pid_t	pid;
 
-	if (prepare_cmd_for_exec(node, envp))
+	if (prepare_cmd_for_exec(node, shell_data->env_vars))
 		return (-1);
 	pid = fork();
 	if (!pid)
 	{
-		execve(node->command->args[0], node->command->args, envp);
+		execve(node->command->args[0], node->command->args, shell_data->env_vars);
 		perror("execve");
 		exit(1);
 	}
@@ -42,7 +42,7 @@ int	execute_command(t_ast_node *node, char **envp)
 	return (-1);
 }
 
-int	execute_redirection(t_ast_node *node, char **envp, int status,
+int	execute_redirection(t_ast_node *node, t_shell_data *shell_data, int status,
 		t_ast_node *cmd_node)
 {
 	int	fd;
@@ -67,12 +67,12 @@ int	execute_redirection(t_ast_node *node, char **envp, int status,
 		if (handle_out_app_redirections(node, fd, saved_stdin) == -1)
 			return (-1);
 	}
-	status = execute_ast(cmd_node, envp);
+	status = execute_ast(cmd_node, shell_data);
 	restore_fds(saved_stdin, saved_stdout);
 	return (status);
 }
 
-int	execute_pipeline(t_ast_node *node, char **envp, int status)
+int	execute_pipeline(t_ast_node *node, t_shell_data *shell_data, int status)
 {
 	int		pipe_fd[2];
 	pid_t	pid1;
@@ -89,26 +89,26 @@ int	execute_pipeline(t_ast_node *node, char **envp, int status)
 	if (kill_on_error(pid1, 1) == -1)
 		return (-1);
 	if (pid1 == 0)
-		exec_first_child(pipe_fd, node, envp);
+		exec_first_child(pipe_fd, node, shell_data);
 	pid2 = fork();
 	if (kill_on_error(pid2, 2) == -1)
 		return (-1);
 	if (pid2 == 0)
-		exec_second_child(pipe_fd, node, envp);
+		exec_second_child(pipe_fd, node, shell_data);
 	close_and_await(pipe_fd, pid1, pid2, status);
 	return (WEXITSTATUS(status));
 }
 
-int	execute_ast(t_ast_node *node, char **envp)
+int	execute_ast(t_ast_node *node, t_shell_data *shell_data)
 {
 	if (!node)
 		return (-1);
 	preprocess_heredocs(node);
 	if (node->type == REDIRECT_NODE)
-		return (execute_redirection(node, envp, -1, NULL));
+		return (execute_redirection(node, shell_data, -1, NULL));
 	if (node->type == COMMAND_NODE)
-		return (execute_command(node, envp));
+		return (execute_command(node, shell_data));
 	if (node->type == PIPE_NODE)
-		return (execute_pipeline(node, envp, -1));
+		return (execute_pipeline(node, shell_data, -1));
 	return (-1);
 }
--- src/main.c
+++ src/main.c
@@ -6,18 +6,22 @@
 /*   By: agarbacz <agarbacz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/28 18:09:03 by agarbacz          #+#    #+#             */
-/*   Updated: 2025/02/10 17:46:44 by agarbacz         ###   ########.fr       */
+/*   Updated: 2025/02/11 11:49:26 by agarbacz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
 
-void	shell_loop(char **envp)
+// env var parsing
+// if '$' before token
+// swap with actual var value (NULL on non-existent)
+void	shell_loop(t_shell_data *shell_data)
 {
 	char	*line;
 	char	**tokens;
 
-	// (void) envp;
+	// (void) shell_data;
+	
 	line = NULL;
 	while (1)
 	{
@@ -25,7 +29,7 @@ void	shell_loop(char **envp)
 		add_history(line);
 		tokens = tokenize(line);
 		t_ast_node *node = build_ast(tokens);
-		execute_ast(node, envp);
+		execute_ast(node, shell_data);
 		// print_ast(node, 0);
 		free_ast(node);
 		free(line);
@@ -38,32 +42,43 @@ void	shell_loop(char **envp)
 // search and lanuch executable
 // here doc not printing and here doc redirecitons maybeeeee
 // error handling?
-int	main(int ac, char **av, char **envp)
-{
-	(void)envp;
-	(void)av;
-	if (ac != 1)
-	{
-		printf("valid exec.: ./minishell");
-		return (0);
-	}
-	shell_loop(envp);
-	return (EXIT_SUCCESS);
-}
-
-// KUBA's MAIN
 // int	main(int ac, char **av, char **envp)
 // {
+// 	(void)envp;
 // 	(void)av;
-// 	t_shell_data shell_data;
-	
-// 	shell_data.env_vars = ft_2d_strdup(envp);
 // 	if (ac != 1)
 // 	{
 // 		printf("valid exec.: ./minishell");
 // 		return (0);
 // 	}
-// 	shell_loop(shell_data);
-// 	ft_arr2d_free(shell_data.env_vars);
+// 	shell_loop(envp);
 // 	return (EXIT_SUCCESS);
 // }
+
+// KUBA's MAIN
+int	main(int ac, char **av, char **envp)
+{
+	(void)av;
+	t_shell_data *shell_data;
+	
+	shell_data = malloc(sizeof(t_shell_data));
+	if (!shell_data)
+		return (1);
+	shell_data->env_vars = ft_2d_strdup(envp);
+	
+	// debug
+	
+	// for (int i = 0; shell_data->env_vars[i]; i++)
+	// {
+	// 	printf("%s\n", shell_data->env_vars[i]);
+	// }
+	if (ac != 1)
+	{
+		printf("valid exec.: ./minishell");
+		return (1);
+	}
+	shell_loop(shell_data);
+	ft_arr2d_free(shell_data->env_vars);
+	free(shell_data);
+	return (EXIT_SUCCESS);
+}
