919a00db66b140b9af349fd8077a775b24bb3702
Aleksander Garbacz <agarbacz@c4r1s5.42warsaw.pl>
2025-02-11 13:47:50 +0100

    env vars + builtin integration

--- inc/minishell.h
+++ inc/minishell.h
@@ -4,9 +4,8 @@
 # define _GNU_SOURCE
 # include "../lib/libft/gnl/get_next_line.h"
 # include "../lib/libft/libft.h"
-# include "parser.h"
 # include "built_ins.h"
-
+# include "parser.h"
 # include <errno.h>
 # include <fcntl.h>
 # include <readline/history.h>
@@ -37,19 +36,20 @@ int			handle_heredoc(char *limiter);
 
 // SECTION: pipes
 int			kill_on_error(pid_t pid, int mode);
-void		exec_first_child(int pipe_fd[2], t_ast_node *node, t_shell_data *shell_data);
-void		exec_second_child(int pipe_fd[2], t_ast_node *node, t_shell_data *shell_data);
+void		exec_first_child(int pipe_fd[2], t_ast_node *node,
+				t_shell_data *shell_data);
+void		exec_second_child(int pipe_fd[2], t_ast_node *node,
+				t_shell_data *shell_data);
 void		close_and_await(int pipe_fd[2], int pid1, int pid2, int status);
 
 // SECTION: execution
 char		**get_path_env_var(char **envp);
 char		*validate_command(char *cmd, char **dirs);
-int			prepare_cmd_for_exec(t_ast_node *node, char **envp);
+int			prepare_cmd_for_exec(t_ast_node *node, t_shell_data *shell_data);
 int			calc_file_flags(t_ast_node *node);
 int			execute_ast(t_ast_node *node, t_shell_data *shell_data);
 
 // SECTION: exec utils
-// int			is_builtin(t_ast_node node, t_shell_data *shell_data);
-int			is_builtin(char *cmd);
+int			is_builtin(t_ast_node node, t_shell_data *shell_data);
 
 #endif
