f6090cc431d98cfe1bf5a02464892f82eeb452c3
jzackiew <jzackiew@student.42warsaw.pl>
2025-02-12 10:32:46 +0100

    norm cleanup

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: jzackiew <jzackiew@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/28 18:09:03 by agarbacz          #+#    #+#             */
-/*   Updated: 2025/02/12 10:13:11 by jzackiew         ###   ########.fr       */
+/*   Updated: 2025/02/12 10:20:21 by jzackiew         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,26 +18,19 @@ void	shell_loop(t_shell_data *shell_data)
 	char		**tokens;
 	t_ast_node	*node;
 
-	
-	line = NULL;
 	while (1)
 	{
-		
 		line = readline("minicfel $> ");
 		if (!line)
 			break;
 		if (!*line)
 			continue;
-		printf("debuf\n");
-		// if (!line)
-		// 	continue;
 		add_history(line);
 		tokens = tokenize(line);
 		if (!tokens || !*tokens || !**tokens)
 			continue;
 		node = build_ast(tokens);
 		execute_ast(node, shell_data);
-		// print_ast(node, 0);
 		free_ast(node);
 		free(line);
 	}
@@ -45,7 +38,6 @@ void	shell_loop(t_shell_data *shell_data)
 }
 
 // TODO:
-// Signal handling
 // here doc not printing and here doc redirecitons
 // heredoc tmp_file not unlinking itself
 // input redirections fixes
@@ -53,7 +45,7 @@ void	shell_loop(t_shell_data *shell_data)
 // error handling?
 // cleanup on exits
 // memory leaks
-// segfault on empty line
+
 int	main(int ac, char **av, char **envp)
 {
 	(void)av;
