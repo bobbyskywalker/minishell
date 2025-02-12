2436bd00002bcf7e7ef92ca2db9040b991d02078
Jzackiewicz <jj.zackiewicz@gmail.com>
2025-02-12 11:14:30 +0100

    started freeing stuff

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: jzackiew <jzackiew@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/28 18:09:03 by agarbacz          #+#    #+#             */
-/*   Updated: 2025/02/12 10:20:21 by jzackiew         ###   ########.fr       */
+/*   Updated: 2025/02/12 11:10:18 by jzackiew         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,12 +27,13 @@ void	shell_loop(t_shell_data *shell_data)
 			continue;
 		add_history(line);
 		tokens = tokenize(line);
+		free(line);
 		if (!tokens || !*tokens || !**tokens)
 			continue;
 		node = build_ast(tokens);
+		free(tokens);
 		execute_ast(node, shell_data);
 		free_ast(node);
-		free(line);
 	}
 	rl_clear_history();
 }
