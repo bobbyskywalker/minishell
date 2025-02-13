f015d7e436bc4552dbdb4a5156f9d2457053fe05
Aleksander Garbacz <agarbacz@c3r11s2.42warsaw.pl>
2025-02-13 16:18:31 +0100

    leak fixes again lol

--- src/exec/pipe.c
+++ src/exec/pipe.c
@@ -6,7 +6,7 @@
 /*   By: agarbacz <agarbacz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/10 16:28:09 by agarbacz          #+#    #+#             */
-/*   Updated: 2025/02/11 13:44:15 by agarbacz         ###   ########.fr       */
+/*   Updated: 2025/02/13 13:56:04 by agarbacz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,22 +34,34 @@ int	kill_on_error(pid_t pid, int mode)
 	return (0);
 }
 
-void	exec_first_child(int pipe_fd[2], t_ast_node *node,
-		t_shell_data *shell_data)
+void exec_first_child(int pipe_fd[2], t_ast_node *node, t_shell_data *shell_data)
 {
-	close(pipe_fd[0]);
-	dup2(pipe_fd[1], STDOUT_FILENO);
-	close(pipe_fd[1]);
-	exit(execute_ast(node->left_child, shell_data));
+	int	status;
+
+    close(pipe_fd[0]);
+    dup2(pipe_fd[1], STDOUT_FILENO);
+    close(pipe_fd[1]);
+    status = execute_ast(node->left_child, shell_data);
+    ft_arr2d_free(shell_data->env_vars);
+    free_ast(shell_data->root);
+    free(shell_data);
+    rl_clear_history();
+    exit(status);
 }
 
-void	exec_second_child(int pipe_fd[2], t_ast_node *node,
-		t_shell_data *shell_data)
+void exec_second_child(int pipe_fd[2], t_ast_node *node, t_shell_data *shell_data)
 {
-	close(pipe_fd[1]);
-	dup2(pipe_fd[0], STDIN_FILENO);
-	close(pipe_fd[0]);
-	exit(execute_ast(node->right_child, shell_data));
+	int	status;
+	
+    close(pipe_fd[1]);
+    dup2(pipe_fd[0], STDIN_FILENO);
+    close(pipe_fd[0]);
+	status = execute_ast(node->right_child, shell_data);
+    ft_arr2d_free(shell_data->env_vars);
+    free_ast(shell_data->root);
+    free(shell_data);
+    rl_clear_history();
+    exit(status);
 }
 
 void	close_and_await(int pipe_fd[2], int pid1, int pid2, int status)
