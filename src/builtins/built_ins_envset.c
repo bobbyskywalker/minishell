e349ee2aa66246e7131f476d418f11b5e0cc3ccb
Jzackiewicz <jj.zackiewicz@gmail.com>
2025-02-14 12:15:23 +0100

    export leak fix

--- src/builtins/built_ins_envset.c
+++ src/builtins/built_ins_envset.c
@@ -3,34 +3,67 @@
 /*                                                        :::      ::::::::   */
 /*   built_ins_envset.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: agarbacz <agarbacz@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jzackiew <jzackiew@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/06 15:11:17 by jzackiew          #+#    #+#             */
-/*   Updated: 2025/02/11 13:44:44 by agarbacz         ###   ########.fr       */
+/*   Updated: 2025/02/14 12:15:08 by jzackiew         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/minishell.h"
 
+void	remove_char(char *str, char c)
+{
+	size_t	i;
+	size_t	j;
+
+	if (!str)
+		return ;
+	i = 0;
+	j = 0;
+	while (i < ft_strlen(str))
+	{
+		if (str[i] != c)
+		{
+			str[j] = str[i];
+			j++;
+		}
+		i++;
+	}
+	str[j] = 0;
+}
+
+static char	*process_args(char *input, t_shell_data shell_data)
+{
+	char	*processed_env;
+	int		id;
+	
+	id = is_key_in_envs(input, shell_data.env_vars);
+	// remove_char(input, '"');
+	// ft_printf("removed: %s\n", input);
+	if ((!ft_strchr(input, '=') || !&ft_strchr(input, '=')[1]) && id >= 0)
+		return (NULL);
+	if (!ft_strchr(input, '='))
+		processed_env = ft_strjoin(input, "=");
+	else
+		processed_env = ft_strdup(input);
+	return (processed_env);
+}
+
 static void	append_env(char *env, t_shell_data *shell_data)
 {
 	int		id;
-	char	*tmp;
 	size_t	vars_count;
 
+	if (!env)
+		return ;
 	id = is_key_in_envs(env, shell_data->env_vars);
-	if (!ft_strchr(env, '=') && id == -1)
-	{
-		tmp = env;
-		env = ft_strjoin(tmp, "=");
-		free(tmp);
-	}
-	else if (id >= 0)
+	if (id >= 0)
 	{
 		free(shell_data->env_vars[id]);
 		shell_data->env_vars[id] = env;
 	}
-	if (id == -1)
+	else
 	{
 		vars_count = ft_2d_strlen(shell_data->env_vars);
 		shell_data->env_vars = ft_2dstr_realloc(shell_data->env_vars, vars_count
@@ -64,7 +97,7 @@ static void	remove_env(char *env, t_shell_data *shell_data)
 int	ft_export(char **args, t_shell_data *shell_data)
 {
 	size_t	i;
-
+	char	*tmp;
 	if (ft_2d_strlen(args) < 1)
 		return (ft_env(*shell_data), 1);
 	i = -1;
@@ -73,7 +106,11 @@ int	ft_export(char **args, t_shell_data *shell_data)
 			return (ft_printf("export: not valid in this context:\n"), -1);
 	i = -1;
 	while (args[++i])
-		append_env(args[i], shell_data);
+	{
+		tmp = process_args(args[i], *shell_data);
+		// tmp = ft_strdup(args[i]);
+		append_env(tmp, shell_data);
+	}
 	return (1);
 }
 
