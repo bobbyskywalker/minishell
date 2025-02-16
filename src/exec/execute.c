19fcf109e3de3e0661093f3d520f0d0d666ccf3c
Jzackiewicz <jj.zackiewicz@gmail.com>
2025-02-17 00:31:52 +0100

    signals refactor

--- src/exec/execute.c
+++ src/exec/execute.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   execute.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jzackiew <jzackiew@student.42.fr>          +#+  +:+       +#+        */
+/*   By: kubaz <kubaz@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/28 18:09:01 by agarbacz          #+#    #+#             */
-/*   Updated: 2025/02/14 18:19:44 by jzackiew         ###   ########.fr       */
+/*   Updated: 2025/02/16 23:31:45 by kubaz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,7 +40,7 @@ int	execute_command(t_ast_node *node, t_shell_data *shell_data)
 	cmd_status = prepare_cmd_for_exec(node, shell_data);
 	if (handle_cmd_errors(cmd_status, shell_data, node) != 0)
 		return (-1);
-	set_signal4child();
+	set_child_signals();
 	pid = fork();
 	if (!pid)
 	{	
